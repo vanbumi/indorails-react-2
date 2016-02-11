5c9419bd7a2258c5d043d984e187856ef163df02
vanbumi <jsp.dio@gmail.com>
2016-02-12 00:05:02 +0700

    first commit

---
+++ app/controllers/lesson_controller.rb
@@ -0,0 +1,17 @@
+class LessonController < ApplicationController
+  def index
+  end
+
+  def chapter_one
+  end
+
+  def chapter_two
+  end
+
+  def chapter_three
+  end
+
+  def chapter_four
+  end
+
+end
